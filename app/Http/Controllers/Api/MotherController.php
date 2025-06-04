f0581b2175a5d87b37525e817b672d9f1e89f907
Alex <alwong@ya.ru>
2025-06-04 05:12:12 +0400

    fix bug

--- app/Http/Controllers/Api/MotherController.php
+++ app/Http/Controllers/Api/MotherController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\ScheduleDay;
+use App\Http\Resources\ScheduleResource;
 
 class MotherController extends Controller
 {
